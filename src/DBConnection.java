e6aedd5d3a8f66704a3bde2814cf81e0a2117587
Hitesh Kumar <kumhit871@gmail.com>
2026-05-04 18:56:42 +0530

    Update DBConnection.java

--- src/DBConnection.java
+++ src/DBConnection.java
@@ -1,17 +1,19 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DBConnection {
-    public static Connection getConnection(){
-        try{
+
+    public static Connection getConnection() {
+        try {
             return DriverManager.getConnection(
                     "jdbc:mysql://localhost:3306/inventory_db",
-                    "root",
-                    "123456"
+                    System.getenv("DB_USER"),
+                    System.getenv("DB_PASS")
             );
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
+
+        } catch (SQLException e) {
+            throw new RuntimeException("Database connection failed", e);
         }
     }
 }
